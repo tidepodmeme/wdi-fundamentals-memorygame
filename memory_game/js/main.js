a511e69ba1bcd5e3a41491862641d3da27fde6f1
~) <clairvoyance@MacBook-Air.local>
2019-01-02 20:03:35 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,25 +23,38 @@ var cards = [
 }
 ];
 
-cardsInPlay = [];
+var cardsInPlay = [];
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");
+	alert("You found a match!");
 	} else {
-	console.log("Sorry, try again.");
+	alert("Sorry, try again.");
 	}
 };
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+
+	this.setAttribute('src', cards[cardId].cardImage);
+
+	if (cardsInPlay.length === 2) {
 	checkForMatch();
+}
 };
 
-flipCard(0);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
 
-flipCard(2);
-
-cardsInPlay.push(cards[cardId].rank);
+createBoard();
